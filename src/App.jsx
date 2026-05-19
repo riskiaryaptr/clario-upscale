631fdb76381fdd3cf621bdb3c5640e071eeda5bb
riskiaryaptr <riskiary1402com>
2026-05-19 23:13:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ function App() {
         <Router>
             <ScrollToTop />
             <Routes>
-                <Route path="/" element={<Index />} />
-                <Route path="/index" element={<Index />} />
+                <Route path="/" element={<Index />}/>
+                <Route path="/index" element={<Index />}/>
                 <Route path="/Pricing" element={<Pricing />} />
-                <Route path="/Reimagine" element={<ReimagineUpscaler />} />
+                <Route path="/Reimagine" element={<ReimagineUpscaler />}/>
                 <Route path="/face-blur" element={<FaceBlur />} />
-                <Route path="/background-remover" element={<BackgroundRemover />} />
-                <Route path="/Login" element={<Login />} />
-                <Route path="/ForgotPassword" element={<ForgotPassword />} />
-                <Route path="/Register" element={<Register />} />
+                <Route path="/background-remover" element={<BackgroundRemover />}/>
+                <Route path="/Login" element={<Login />}/>
+                <Route path="/ForgotPassword" element={<ForgotPassword />}/>
+                <Route path="/Register" element={<Register />}/>
             </Routes>
         </Router>
     );

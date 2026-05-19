c9253e40f8e6d1be4a21a54e6d548ac365723c27
riskiaryaptr <riskiary1402com>
2026-05-19 23:12:42 +0700

    Update App.jsx and index.html

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,32 @@ import Register from "@/frontend/auth/Register";
 import ForgotPassword from "@/frontend/auth/ForgotPassword";
 
 function ScrollToTop() {
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
 
-  return null;
+    return null;
 }
 
 function App() {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Routes>
-          <Route path="/" element={<Index />} />
-        <Route path="/index" element={<Index />} />
-        <Route path="/Pricing" element={<Pricing />} />
-        <Route path="/Reimagine" element={<ReimagineUpscaler />} />
-        <Route path="/face-blur" element={<FaceBlur />} />
-        <Route path="/background-remover" element={<BackgroundRemover />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/ForgotPassword" element={<ForgotPassword />} />
-        <Route path="/Register" element={<Register />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/index" element={<Index />} />
+                <Route path="/Pricing" element={<Pricing />} />
+                <Route path="/Reimagine" element={<ReimagineUpscaler />} />
+                <Route path="/face-blur" element={<FaceBlur />} />
+                <Route path="/background-remover" element={<BackgroundRemover />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/ForgotPassword" element={<ForgotPassword />} />
+                <Route path="/Register" element={<Register />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
